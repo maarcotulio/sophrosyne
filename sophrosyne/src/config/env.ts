01ce80304d84268957adea8f3b3a33b464e5afc1
Marco <marcotuliotavaresoliveira@gmail.com>
2025-12-30 11:09:40 -0300

    feat: create habit

---
+++ sophrosyne/src/config/env.ts
@@ -0,0 +1,3 @@
+export const env = {
+    HABITS_TABLE: process.env.HABITS_TABLE!,
+}
--- sophrosyne/src/functions/createHabit.ts
+++ sophrosyne/src/functions/createHabit.ts
@@ -1,6 +1,45 @@
 import type { APIGatewayProxyEventV2 } from "aws-lambda";
 import { response } from "../utils/response.js";
+import zod from "zod";
+import { dynamoClient } from '../clients/dynamoClients.js';
+import { PutCommand } from '@aws-sdk/lib-dynamodb';
+
+const schema = zod.object({
+    habitName: zod.string(),
+    habitDescription: zod.string()
+})
 
 export async function handler(event: APIGatewayProxyEventV2) {
-    return response(200, { message: 'Hello World' })
+    const { success, data, error} = schema.safeParse(JSON.parse(event.body ?? "{}"));
+
+    const userId = event.pathParameters?.userId;
+
+    if(!userId) {
+        return response(400, { error: "User ID is required" })
+    }
+
+    if(!success) {
+        return response(400, { error: error.message })
+    }
+
+    // TODO: Verify if user exists
+
+    const { habitName, habitDescription } = data;
+    const id = crypto.randomUUID();
+    const createdAt = new Date().toISOString();
+
+    const command = new PutCommand({
+        TableName: process.env.HABITS_TABLE,
+        Item: {
+            id,
+            userId,
+            habitName,
+            habitDescription,
+            createdAt,
+        },
+    });
+
+    await dynamoClient.send(command);
+
+    return response(201, { id })
 }
