c7ecea00dba81f0e0fedd9549baf0f1c806f853a
Marco <marcotuliotavaresoliveira@gmail.com>
2026-01-01 14:16:30 -0300

    feat: list habits

---
+++ sophrosyne/src/functions/habits/listHabit.ts
@@ -0,0 +1,25 @@
+import type { APIGatewayProxyEventV2WithJWTAuthorizer } from 'aws-lambda';
+import { response } from '../../utils/response.js';
+import { dynamoClient } from '../../clients/dynamoClients.js';
+import { QueryCommand } from '@aws-sdk/lib-dynamodb';
+
+export async function handler(event: APIGatewayProxyEventV2WithJWTAuthorizer) {
+    const userId = event.requestContext.authorizer.jwt.claims.sub as string;
+
+    if (!userId) {
+        return response(400, { error: 'User ID is required' });
+    }
+
+    const { Items: habits } = await dynamoClient.send(
+        new QueryCommand({
+            TableName: process.env.SOPHROSYNE,
+            KeyConditionExpression: 'PK = :pk AND begins_with(SK, :sk)',
+            ExpressionAttributeValues: {
+                ':pk': `USER#${userId}`,
+                ':sk': 'HABIT#',
+            },
+        })
+    );
+
+    return response(200, { habits: habits ?? [] });
+}
