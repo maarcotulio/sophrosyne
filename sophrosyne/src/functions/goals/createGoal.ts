455d5bba0361984cca556230076b8b7d2b92c8a9
Marco <marcotuliotavaresoliveira@gmail.com>
2026-01-03 12:59:49 -0300

    feat: createGoal function

---
+++ sophrosyne/src/functions/goals/createGoal.ts
@@ -0,0 +1,38 @@
+import type { APIGatewayProxyEventV2WithJWTAuthorizer } from 'aws-lambda';
+import { goalSchema } from '../../schemas/goalSchema.js';
+import { response } from '../../utils/response.js';
+import { PutCommand } from '@aws-sdk/lib-dynamodb';
+import { dynamoClient } from '../../clients/dynamoClients.js';
+
+export async function handler(event: APIGatewayProxyEventV2WithJWTAuthorizer) {
+    const { success, data, error } = goalSchema.safeParse(
+        JSON.parse(event.body ?? '{}')
+    );
+
+    const userId = event.requestContext.authorizer.jwt.claims.sub as string;
+
+    if (!success) {
+        return response(400, { error: error.message });
+    }
+
+    const { name, description } = data;
+
+    const goalId = crypto.randomUUID();
+    const goal = {
+        PK: `USER#${userId}`,
+        SK: `GOAL#${goalId}`,
+        id: goalId,
+        name,
+        description,
+        createdAt: new Date().toISOString(),
+    };
+
+    const command = new PutCommand({
+        TableName: process.env.SOPHROSYNE,
+        Item: goal,
+    });
+
+    await dynamoClient.send(command);
+
+    return response(201, { id: goal.id });
+}
