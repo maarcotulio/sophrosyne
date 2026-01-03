121a61ae82d5f2d4426743f23688c262c813cf98
Marco <marcotuliotavaresoliveira@gmail.com>
2026-01-03 13:12:44 -0300

    feat: listGoal

---
+++ sophrosyne/src/functions/goals/listGoal.ts
@@ -0,0 +1,21 @@
+import type { APIGatewayProxyEventV2WithJWTAuthorizer } from 'aws-lambda';
+import { response } from '../../utils/response.js';
+import { ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { dynamoClient } from '../../clients/dynamoClients.js';
+
+export async function handler(event: APIGatewayProxyEventV2WithJWTAuthorizer) {
+    const userId = event.requestContext.authorizer.jwt.claims.sub as string;
+
+    const command = new ScanCommand({
+        TableName: process.env.SOPHROSYNE,
+        FilterExpression: 'PK = :userId AND begins_with(SK, :sk)',
+        ExpressionAttributeValues: {
+            ':userId': `USER#${userId}`,
+            ':sk': 'GOAL#',
+        },
+    });
+
+    const { Items: goals } = await dynamoClient.send(command);
+
+    return response(200, { goals });
+}
