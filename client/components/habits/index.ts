2bb012bfa11b4c2640298acf6ca0bb84bae6e768
Marco <marcotuliotavaresoliveira@gmail.com>
2026-01-11 19:38:46 -0300

    feat(habits): create index.ts barrel export file

---
+++ client/components/habits/index.ts
@@ -0,0 +1,6 @@
+export { HabitCard } from "./habit-card";
+export { HabitsHeader } from "./habits-header";
+export { HabitsSearchBar } from "./habits-search-bar";
+export { NewHabitForm } from "./new-habit-form";
+export { CreateHabitCard } from "./create-habit-card";
+export type { HabitData } from "./types";
