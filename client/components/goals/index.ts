2e1b0b7264a502fddb4d52957a95e81c67fa33f1
Marco <marcotuliotavaresoliveira@gmail.com>
2026-01-12 10:19:07 -0300

    feat(goals): create index.ts barrel export file

---
+++ client/components/goals/index.ts
@@ -0,0 +1,6 @@
+export { GoalsHeader } from "./goals-header";
+export { GoalsStatCard } from "./goals-stat-card";
+export { GoalsTabNav } from "./goals-tab-nav";
+export { GoalCard } from "./goal-card";
+export { CreateGoalCard } from "./create-goal-card";
+export type { GoalData, LinkedHabit, GoalStatData } from "./types";
